c360be6f9d7f6591a126fb79417fe2a458e773c8
王高 <420884853@qq.com>
2025-03-02 10:32:52 +0800

    解决分页没有携带状态参数的问题

--- src/components/WarningEvent.vue
+++ src/components/WarningEvent.vue
@@ -20,7 +20,8 @@
               v-for="(item, index) in nameOptions"
               :key="index"
               :command="item"
-              >{{ item }}</el-dropdown-item
+            >{{ item }}
+            </el-dropdown-item
             >
           </el-dropdown-menu>
         </el-dropdown>
@@ -36,7 +37,8 @@
               :key="index"
               :command="item"
               :class="{'dropdown_selected': eventStatus == item}"
-              >{{ item }}</el-dropdown-item
+            >{{ item }}
+            </el-dropdown-item
             >
           </el-dropdown-menu>
         </el-dropdown>
@@ -47,13 +49,16 @@
       </template>
       <template #status="{ row }">
         <el-tag :type="statusType(row.status)">{{
-          statusTitle(row.status)
-        }}</el-tag>
+            statusTitle(row.status)
+          }}
+        </el-tag>
       </template>
       <template #operate="{ row }">
         <div class="operate-box">
           <el-button type="text" @click="detailsBtn(row)" v-permissions="'wurenji:warning:list'">详情</el-button>
-          <el-dropdown @command="operateCommand(row)" v-show="row.status == '0'" v-permissions="'wurenji:warning:updateStatus'">
+          <el-dropdown @command="operateCommand(row)" v-show="row.status == '0'"
+                       v-permissions="'wurenji:warning:updateStatus'"
+          >
             <span class="el-dropdown-link el-icon-more"></span>
             <el-dropdown-menu slot="dropdown">
               <el-dropdown-item
@@ -62,8 +67,9 @@
                 :key="index"
                 :command="item.label"
                 :style="{ color: item.color }"
-                ><i class="iconfont" :class="item.icon"></i>
-                {{ item.label }}</el-dropdown-item
+              ><i class="iconfont" :class="item.icon"></i>
+                {{ item.label }}
+              </el-dropdown-item
               >
             </el-dropdown-menu>
           </el-dropdown>
@@ -82,7 +88,8 @@
               font-size: 14px;
             "
             @click="flightBtn(row)"
-            >处理</el-button
+          >处理
+          </el-button
           >
 
         </div>
@@ -117,15 +124,16 @@
 </template>
 
 <script>
-import CommonTable from "./CommonTable.vue";
-import { mockList } from "@/utils/mock.js";
-import WarningDialog from "./Template/WarningDialog.vue";
-import EventInfo from "./Template/EventInfo.vue";
-import NeglectDialog from "./Template/NeglectDialog.vue";
-import HandleDialog from "./Template/HandleDialog.vue";
-import { warningListAPI } from "@/api/TaskManager.js";
+import CommonTable from './CommonTable.vue'
+import { mockList } from '@/utils/mock.js'
+import WarningDialog from './Template/WarningDialog.vue'
+import EventInfo from './Template/EventInfo.vue'
+import NeglectDialog from './Template/NeglectDialog.vue'
+import HandleDialog from './Template/HandleDialog.vue'
+import { warningListAPI } from '@/api/TaskManager.js'
+
 export default {
-  name: "FlightTable",
+  name: 'FlightTable',
   props: {},
   data() {
     return {
@@ -137,57 +145,57 @@ export default {
       total: 0,
       columns: [
         {
-          prop: "warnName",
-          label: "事件名称",
+          prop: 'warnName',
+          label: '事件名称',
           showOverflowTooltip: true,
           slot: true,
-          minWidth: "200",
+          minWidth: '200'
         },
         {
-          prop: "identifyAirPortName",
-          label: "机场",
-          showOverflowTooltip: true,
+          prop: 'identifyAirPortName',
+          label: '机场',
+          showOverflowTooltip: true
         },
         {
-          prop: "identifyTime",
-          label: "发现时间",
+          prop: 'identifyTime',
+          label: '发现时间',
+          showOverflowTooltip: false
+        },
+        {
+          prop: 'status',
+          label: '本轮状态',
           showOverflowTooltip: false,
+          slot: true
         },
         {
-          prop: "status",
-          label: "本轮状态",
+          prop: 'operate',
+          label: '操作',
           showOverflowTooltip: false,
-          slot: true,
-        },
-        {
-          prop: "operate",
-          label: "操作",
-          showOverflowTooltip: false,
-          width: "200px",
-          slot: true,
-        },
+          width: '200px',
+          slot: true
+        }
       ],
       nameOptions: [
-        "全部类型",
-        "拍照",
-        "直播",
-        "全景",
-        "三维",
-        "正射",
-        "全覆盖",
+        '全部类型',
+        '拍照',
+        '直播',
+        '全景',
+        '三维',
+        '正射',
+        '全覆盖'
       ],
       statusOptions: [
-        "全部状态",
-        "新发现",
-        "已办结",
-        "已忽略",
+        '全部状态',
+        '新发现',
+        '已办结',
+        '已忽略'
       ],
       operateOptions: [
         {
-          label: "忽略",
-          icon: "el-icon-hulve",
-          checked: false,
-        },
+          label: '忽略',
+          icon: 'el-icon-hulve',
+          checked: false
+        }
         // {
         //   label: "设为典型",
         //   icon: "el-icon-xingxing",
@@ -197,102 +205,102 @@ export default {
       pageSize: 10,
       warningList: [],
       itemRow: {},
-      warnName: ""
-    };
+      warnName: ''
+    }
   },
   computed: {
     statusType() {
-      return function (status) {
+      return function(status) {
         switch (status) {
-          case "0":
-            return "warning";
-          case "5":
+          case '0':
+            return 'warning'
+          case '5':
             return 'info'
           default:
-            break;
+            break
         }
-      };
+      }
     },
     statusTitle(status) {
-      return function (status) {
+      return function(status) {
         switch (status) {
-          case "0":
-            return "新发现";
-          case "1":
-            return "已上报";
-          case "2":
-            return "已受理";
-          case "3":
-            return "已办结";
-          case "4":
-            return "已超期";
-          case "5":
-            return "已忽略";
-          case "6":
-            return "典型";
-          case "7":
-            return "取消典型";
-          case "8":
-            return "审核";
+          case '0':
+            return '新发现'
+          case '1':
+            return '已上报'
+          case '2':
+            return '已受理'
+          case '3':
+            return '已办结'
+          case '4':
+            return '已超期'
+          case '5':
+            return '已忽略'
+          case '6':
+            return '典型'
+          case '7':
+            return '取消典型'
+          case '8':
+            return '审核'
           default:
-            return "";
+            return ''
         }
-      };
+      }
     },
     statusTypes(status) {
-      return function (status) {
+      return function(status) {
         switch (status) {
-          case "新发现":
-            return '0';
-          case "已上报":
-            return '1';
-          case "已受理":
-            return '2';
-          case "已办结":
-            return '3';
-          case "已超期":
-            return '4';
-          case "已忽略":
-            return '5';
+          case '新发现':
+            return '0'
+          case '已上报':
+            return '1'
+          case '已受理':
+            return '2'
+          case '已办结':
+            return '3'
+          case '已超期':
+            return '4'
+          case '已忽略':
+            return '5'
           default:
-            return '';
+            return ''
         }
-      };
-    },
+      }
+    }
   },
   mounted() {
-    this.getWarningList();
+    this.getWarningList()
   },
   methods: {
     searchWarningName(val) {
-      this.warnName = val;
-      this.pageNum = 1;
+      this.warnName = val
+      this.pageNum = 1
       const params = {
         pageNum: this.pageNum,
         pageSize: this.pageSize,
         orgId: localStorage.getItem('org_id'),
         warnName: val,
         status: this.statusTypes(this.eventStatus)
-      };
+      }
       warningListAPI(params).then((res) => {
         if (res.code === 200) {
-          this.warningList = res.rows;
-          this.total = res.total;
+          this.warningList = res.rows
+          this.total = res.total
         }
-      });
+      })
     },
     updateList() {
-      this.getWarningList();
+      this.getWarningList()
     },
     pageChange(val) {
-      this.pageSize = val.pageSize;
-      this.pageNum = val.pageNum;
-      this.getWarningList();
+      this.pageSize = val.pageSize
+      this.pageNum = val.pageNum
+      this.getWarningList()
     },
     sizeChange(val) {
-      this.pageNum = val.pageNum;
-      this.pageSize = val.pageSize;
-      this.getWarningList();
+      this.pageNum = val.pageNum
+      this.pageSize = val.pageSize
+      this.getWarningList()
     },
     getWarningList() {
       const params = {
@@ -300,63 +308,64 @@ export default {
         pageSize: this.pageSize,
         orgId: localStorage.getItem('org_id'),
         warnName: this.warnName,
-      //  status: this.statusTypes(this.eventStatus)
-      };
+        status: this.statusTypes(this.eventStatus)
+      }
       warningListAPI(params).then((res) => {
         if (res.code === 200) {
-          this.warningList = res.rows;
-          this.total = res.total;
+          this.warningList = res.rows
+          this.total = res.total
         }
-      });
+      })
     },
     closeNeglectDialog() {
-      this.neglectVisible = false;
+      this.neglectVisible = false
     },
     closeWarningDialog() {
-      this.warningVisible = false;
+      this.warningVisible = false
     },
     nameCommand(itemCommand) {
     },
     statusCommand(itemCommand) {
-      this.eventStatus = itemCommand;
+      this.eventStatus = itemCommand
+      this.pageNum = 0;
       const params = {
         pageNum: this.pageNum,
         pageSize: this.pageSize,
         orgId: localStorage.getItem('org_id'),
         warnName: this.warnName,
-        status: this.statusTypes(itemCommand)
-      };
+        status: this.statusTypes(this.eventStatus)
+      }
       warningListAPI(params).then((res) => {
         if (res.code === 200) {
-          this.warningList = res.rows;
-          this.total = res.total;
+          this.warningList = res.rows
+          this.total = res.total
         }
-      });
+      })
     },
     operateCommand(itemCommand) {
-      this.neglectVisible = true;
-      this.itemRow = itemCommand;
+      this.neglectVisible = true
+      this.itemRow = itemCommand
     },
     detailsBtn(row) {
-      this.itemRow = row;
-      this.warningVisible = true;
+      this.itemRow = row
+      this.warningVisible = true
     },
     flightBtn(row) {
-      this.itemRow = row;
-      this.handleVisible = true;
+      this.itemRow = row
+      this.handleVisible = true
     },
     closeHandleDialog() {
-      this.handleVisible = false;
-    },
+      this.handleVisible = false
+    }
   },
   components: {
     CommonTable,
     WarningDialog,
     EventInfo,
     NeglectDialog,
-    HandleDialog,
-  },
-};
+    HandleDialog
+  }
+}
 </script>
 
 <style lang="scss">
@@ -364,6 +373,7 @@ export default {
   .filter-icon {
     margin-left: 5px;
     vertical-align: text-top;
+
     &:hover {
       cursor: pointer;
       transform: scale(1.3);
